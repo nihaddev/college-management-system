af2963c112895303875ccb76ce14cbd8a483013d
= <kardesdcys@gmail.com>
2026-09-11 02:07:34 +0400

    feat(add): add perm control

--- src/main/java/az/edu/bbkk/backend/controller/AdminController.java
+++ src/main/java/az/edu/bbkk/backend/controller/AdminController.java
@@ -1,9 +1,11 @@
 package az.edu.bbkk.backend.controller;
 
 import az.edu.bbkk.backend.entity.Teacher;
+import az.edu.bbkk.backend.service.AdminService;
 import az.edu.bbkk.backend.service.StudentService;
 import az.edu.bbkk.backend.service.TeacherService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,15 +19,23 @@ import java.util.Map;
 public class AdminController {
     private final StudentService studentService;
     private final TeacherService teacherService;
+    private final AdminService adminService;
 
-    public AdminController(StudentService studentService, TeacherService teacherService) {
+    public AdminController(StudentService studentService, TeacherService teacherService, AdminService adminService) {
         this.studentService = studentService;
         this.teacherService = teacherService;
+        this.adminService = adminService;
     }
 
     // Teacher
     @PostMapping("/teacher/new")
     public ResponseEntity<?> newTeacher(@Valid @RequestBody Teacher teacher) {
+        if (!adminService.isAdmin()) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Unauthorized access. Bu əməliyyat üçün admin hüququ tələb olunur."));
+        }
+
         Teacher newTeacherResponse = teacherService.createTeacher(teacher);
         return ResponseEntity.ok(Map.of(
                 "status", 200,
