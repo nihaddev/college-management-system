4da7728b1f14e56476b1d470300322e46f8d2bfa
= <kardesdcys@gmail.com>
2026-09-11 03:10:09 +0400

    refactor:

--- src/main/java/az/edu/bbkk/backend/controller/AdminController.java
+++ src/main/java/az/edu/bbkk/backend/controller/AdminController.java
@@ -28,7 +28,13 @@ public class AdminController {
 
     // Students
     @GetMapping("/students")
-    public ResponseEntity<?> getAllStudents() {
+    public ResponseEntity<?> getAllStudents(@PathVariable String finCode,@PathVariable String username,@PathVariable String name,@PathVariable String surname) {
+        if (!adminService.isAdmin()) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of("message", "Unauthorized access. Bu əməliyyat üçün admin hüququ tələb olunur."));
+        }
+
         return ResponseEntity.ok(Map.of(
                 "status", 200,
                 "data", studentService.getAllStudents()
